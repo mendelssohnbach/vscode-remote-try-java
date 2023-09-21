ddcbc7ff0b72fe5fa8971bade30b5cdb05f64017
Yasuji Mendel Nakanishi <mendelssohnusaic+github@gmail.com>
2023-09-21 23:35:01 +0000

    Section10

--- src/main/java/com/mycompany/app/Franc.java
+++ src/main/java/com/mycompany/app/Franc.java
@@ -1,10 +1,11 @@
 package com.mycompany.app;
 
-public class Franc extends Money {
-    Franc(int amount) {
-        this.amount = amount;
+class Franc extends Money {
+
+    Franc(int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 }
